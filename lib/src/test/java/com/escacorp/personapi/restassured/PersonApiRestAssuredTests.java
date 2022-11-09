2da7a930704babcb6cc2810cbaa1c4c2eb4f6be8
Alex Escalante <alexnescalante@unicorn.local>
2022-11-08 17:33:15 -0800

    Housekeeping imports

--- lib/src/test/java/com/escacorp/personapi/restassured/PersonApiRestAssuredTests.java
+++ lib/src/test/java/com/escacorp/personapi/restassured/PersonApiRestAssuredTests.java
@@ -1,21 +1,15 @@
 package com.escacorp.personapi.restassured;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
-
-import java.util.List;
-import java.util.Map;
-
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class PersonApiRestAssuredTests {
 	
